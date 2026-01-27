1921b659e74f5c7356f59d7ced803f78054551cd
Ethan Marshall <em2017@cam.ac.uk>
2026-01-27 10:29:41 +0000

    Add library for line follower chip

---
+++ sw/libs/line_follower/DFRobot_SEN0017.py
@@ -0,0 +1,12 @@
+from machine import Pin
+
+'''Simple interface class for the line following sensor.
+   Reserves the pin which the sensor is on for itself (set to read).
+'''
+class DFRobot_SEN0017:
+    def __init__(self, sensPin):
+        self.pin = Pin(sensPin, Pin.IN)
+
+    '''Returns true for the sensor reading white (above a line)'''
+    def on_line(self):
+        return self.pin.value() == 1
