ba2aac526d30b6c5cfa444823f024546ce19b64e
erniepsycholone <erniehyt321@gmail.com>
2026-02-09 14:26:52 +0000

    Minor updates

--- sw/resistance_checker.py
+++ sw/resistance_checker.py
@@ -1,4 +1,4 @@
-import utime
+import time
 
 def resistance(adc0):
     # read ADC input on pin ADC0 as 16-bit integer (0 - 65535)
@@ -6,5 +6,5 @@ def resistance(adc0):
     # Convert analog reading (0 - 65535) to a voltage (0 - 3.3V)
     adc_voltage = adc_value * (3.3/65536)
     # send data to computer over USB
-    utime.sleep(0.5)
+    time.sleep(0.5)
     return(adc_voltage)
